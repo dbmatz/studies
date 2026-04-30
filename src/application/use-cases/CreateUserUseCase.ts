ed57dc66efe628b86849bd665d15bb6c065dfe46
Debora Pacheco <dpacheco@inovvati.com.br>
2026-04-30 15:01:06 -0400

    refactor: update CreateUserUseCase to use DTO and modify UserController accordingly

--- src/application/use-cases/CreateUserUseCase.ts
+++ src/application/use-cases/CreateUserUseCase.ts
@@ -1,12 +1,20 @@
-import { Request } from "express";
 import { IUserRepository } from "../repositories/IUserRepository";
 import { User } from "../../domain/entities/User";
+export interface CreateUserInput {
+  name: string;
+  email: string;
+  password: string;
+}
 
-export class CreateUserUseCase {
+export interface ICreateUserUseCase {
+  execute(dto: CreateUserInput): Promise<User>;
+}
+
+export class CreateUserUseCase implements ICreateUserUseCase {
   constructor(private readonly userRepository: IUserRepository) {}
 
-  async execute(req: Request): Promise<User> {
-    const { email, name, password } = req.body;
+  async execute(dto: CreateUserInput): Promise<User> {
+    const { email, name, password } = dto;
     if (!email || !name || !password) {
       throw new Error("Please provide the required information.");
     }
--- src/infra/http/controllers/UserController.ts
+++ src/infra/http/controllers/UserController.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
-import { CreateUserUseCase } from "../../../application/use-cases/CreateUserUseCase";
+import { ICreateUserUseCase } from "../../../application/use-cases/CreateUserUseCase";
 
 export class UserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor(private createUserUseCase: ICreateUserUseCase) {}
   async createUser(req: Request, res: Response) {
     try {
-      const user = await this.createUserUseCase.execute(req);
+      const { email, name, password } = req.body;
+      const user = await this.createUserUseCase.execute({
+        email,
+        name,
+        password,
+      });
       res.status(201).json({ message: "User created", data: user });
     } catch (error: any) {
       res.status(500).json({ message: error.message });
