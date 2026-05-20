8c7a4fb4de6941f1d31c0c0697b4e9140553be2c
Debora Pacheco <dpacheco@inovvati.com.br>
2026-05-19 21:02:00 -0400

    refactor: update User entity to encapsulate password and improve constructor typing

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -4,12 +4,23 @@ export class User {
   public readonly id: string;
   public name: string;
   public email: string;
-  public password: string;
+  private password: string;
 
-  constructor(props: Omit<User, "id">, id?: string) {
+  constructor(
+    props: { name: string; email: string; password: string },
+    id?: string,
+  ) {
     this.id = id ?? v4();
     this.name = props.name;
     this.email = props.email;
     this.password = props.password;
+  }
+  
+  toJSON() {
+    return {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+    };
   }
 }
